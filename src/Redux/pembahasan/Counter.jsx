409e0cec7eebc66941271b23dc50ffbd53411df6
DESKTOP-HR98PIA\Siti Hajar Soleha <amishbah12@gmail.com>
2023-10-29 14:20:02 +0700

    update

--- src/Redux/pembahasan/Counter.jsx
+++ src/Redux/pembahasan/Counter.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { decrementWithCheckingAction, increment } from "../../app/features/Counter/action";
+import { decrement, increment } from "../../app/features/Counter/action";
 
 const Counter = () => {
   let { count } = useSelector((state) => state.counter);
@@ -7,7 +7,7 @@ const Counter = () => {
 
   return (
     <div>
-      <button onClick={() => dispatch(decrementWithCheckingAction(1))}>-</button>
+      <button onClick={() => dispatch(decrement(1))}>-</button>
       {""} <span>{count}</span> {""}
       <button onClick={() => dispatch(increment(1))}>+</button>
     </div>
